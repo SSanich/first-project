fd2ab81aacf1f037261b119085ffeabc342b04be
Aleksandr Grigoriev <sanich.grig@gmail.com>
2024-03-15 12:00:27 +0700

    change to SSH

--- reading_loop.java.java
+++ reading_loop.java.java
@@ -2,7 +2,7 @@ public class Praktikum {
   public static void main(String[] args) {
         int freeHours = 4; // Объявили переменную с количеством свободного времени в часах и присвоили ей значение
                         
-        checkReading(freeHours); // вызываем метод с циклом и передаём в него количество свободного времени
+        checkReading(freeHours); // вызываем  метод с циклом и передаём в него количество свободного времени
   }
 
   public static void checkReading(int freeHours) {// метод принимает количество свободного времени как параметр
